3b56f5fa45cf7155cf619120514f39e104b8c103
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-05 21:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getURLlength(){
+	let currentURL = location.href;
+	let urlLength = currentURL.length;
+	return urlLength;
+}
+
+alert("The length of the URL is: " + getURLlength()); 
